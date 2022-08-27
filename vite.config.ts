d9f73fbc48c024f801f632472a78a406e62c2cbb
Timothy Cool <tcoolpg@gmail.com>
2022-08-26 20:20:20 -0700

    add rollup options for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
+import alias from '@rollup/plugin-alias';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve('src') }],
+	},
+	build: {
+		rollupOptions: {
+			plugins: [
+				alias({
+					entries: [
+						{ find: '@', replacement: path.resolve('src') }
+					]
+				})
+			]
+		}
 	}
 })
