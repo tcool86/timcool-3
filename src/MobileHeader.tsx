8595bf4c7548fee7ccbe87e72f2089abc8ae8ccc
Timothy Cool <tcoolpg@gmail.com>
2022-08-26 18:56:17 -0700

    revert path alias

--- src/MobileHeader.tsx
+++ src/MobileHeader.tsx
@@ -1,8 +1,8 @@
 import './MobileHeader.css';
 import { useRef } from 'react';
 import { motion, useCycle } from 'framer-motion';
-import { MenuToggle } from '@/menu/MenuToggle';
-import { Menu } from '@/menu/Menu';
+import { MenuToggle } from './menu/MenuToggle';
+import { Menu } from './menu/Menu';
 
 function MobileHeader() {
 	const [isOpen, toggleOpen] = useCycle(false, true);
