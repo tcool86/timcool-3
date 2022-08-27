4560667a96074f814d6aa0c208e1359483458b4e
Timothy Cool <tcoolpg@gmail.com>
2022-08-26 20:07:02 -0700

    just use vite build

--- src/Pages/Archive/Archive.tsx
+++ src/Pages/Archive/Archive.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import './Archive.css';
 
-export function Archive() {
+export default function Archive() {
 	return (
 		<React.Fragment>
 			<motion.h2 layout className="title subtitle">
