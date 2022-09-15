824a95bef8f9288230156d28c4f452fb55a27d8d
Timothy Cool <tcoolpg@gmail.com>
2022-09-14 21:19:42 -0700

    add: social links to footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,11 @@
 import './Footer.css';
 import { motion } from 'framer-motion';
+import SocialLinks from './components/SocialLinks';
 
 function Footer() {
 	return (
 		<motion.footer className="app-footer">
+			<motion.div>{SocialLinks()}</motion.div>
 			<motion.p>Copyright &copy; 2022 - Tim Cool</motion.p>
 		</motion.footer>
 	);
