7f5db8fb94de86122179eba75dd51094e48c6576
Timothy Cool <tcoolpg@gmail.com>
2022-09-17 23:57:59 -0700

    add: social icons for twitter and buy me a coffee. Remove underdeveloped social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,12 +1,6 @@
 import './SocialLinks.css';
 import { motion } from 'framer-motion';
-import {
-	LinkedInIcon,
-	StackOverflowIcon,
-	GithubIcon,
-	DiscordIcon,
-	MediumIcon,
-} from '../assets/Icons';
+import { LinkedInIcon, GithubIcon, TwitterIcon } from '../assets/Icons';
 
 export default function SocialLinks() {
 	const socialLinks = [
@@ -15,26 +9,31 @@ export default function SocialLinks() {
 			name: 'linked-in',
 			icon: LinkedInIcon,
 		},
+		// {
+		// 	link: 'https://stackoverflow.com/users/1347616/tcool',
+		// 	name: 'stackoverflow',
+		// 	icon: StackOverflowIcon,
+		// },
 		{
-			link: 'https://stackoverflow.com/users/1347616/tcool',
-			name: 'stackoverflow',
-			icon: StackOverflowIcon,
+			link: 'https://twitter.com/timothymcool',
+			name: 'twitter',
+			icon: TwitterIcon,
 		},
 		{
 			link: 'https://github.com/tcool86/',
 			name: 'github',
 			icon: GithubIcon,
 		},
-		{
-			link: 'https://discordapp.com/users/349036886813310976',
-			name: 'discord',
-			icon: DiscordIcon,
-		},
-		{
-			link: 'https://tcool.medium.com',
-			name: 'medium-blog',
-			icon: MediumIcon,
-		},
+		// {
+		// 	link: 'https://discordapp.com/users/349036886813310976',
+		// 	name: 'discord',
+		// 	icon: DiscordIcon,
+		// },
+		// {
+		// 	link: 'https://tcool.medium.com',
+		// 	name: 'medium-blog',
+		// 	icon: MediumIcon,
+		// },
 	];
 	return (
 		<motion.ul className="app-social-links">
